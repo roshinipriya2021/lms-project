cfa7c5a7463eaec2737edc30208e7cb1a18466c7
roshinipriya2021 <roshinipriya2021@gmail.com>
2026-06-26 15:29:41 +0530

    Update API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://lms-backend-02t5.onrender.com",
+  baseURL: "https://lms-backend-02t5.onrender.com/api",
 });
 
 export default api;
